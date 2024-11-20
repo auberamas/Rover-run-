a1ce4dc2d95194e44c4c869c190aa3d4adce1361
irmnjk <sophiejaffal@gmail.com>
2024-11-20 16:26:51 +0100

    menu function and some other stuff :)

--- menu.h
+++ menu.h
@@ -8,9 +8,13 @@
 #include "\modified\map.h"
 #include "\modified\loc.h"
 
-void mission();
-void choose_map();
-void choose_localisation();
+static char* Maps[] = {"..\\maps\\exemple1.map", "..\\maps\\training_2.map", "..\\maps\\training_3.map", "..\\maps\\training_4.map", "..\\maps\\training_5.map"};
+int mission();
+void messageEnd(int);
+int userInput(int, int);
+t_map chooseMap();
+t_localisation chooseLocalisation();
+t_orientation chooseOrientation();
 void wantShowMap();
 void wantShowComplexity();
 
